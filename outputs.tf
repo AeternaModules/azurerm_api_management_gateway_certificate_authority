47a2743501520a74c98e4407ddeb5384accfff74
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "api_management_gateway_certificate_authorities" {
   description = "All api_management_gateway_certificate_authority resources"
   value       = azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities
 }
+output "api_management_gateway_certificate_authorities_api_management_id" {
+  description = "List of api_management_id values across all api_management_gateway_certificate_authorities"
+  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.api_management_id]
+}
+output "api_management_gateway_certificate_authorities_certificate_name" {
+  description = "List of certificate_name values across all api_management_gateway_certificate_authorities"
+  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.certificate_name]
+}
+output "api_management_gateway_certificate_authorities_gateway_name" {
+  description = "List of gateway_name values across all api_management_gateway_certificate_authorities"
+  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.gateway_name]
+}
+output "api_management_gateway_certificate_authorities_is_trusted" {
+  description = "List of is_trusted values across all api_management_gateway_certificate_authorities"
+  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.is_trusted]
+}
 
