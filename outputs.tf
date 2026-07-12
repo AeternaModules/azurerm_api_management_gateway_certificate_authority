cfd1102c42660fb74e8d15fb06ea0e715f3596be
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_gateway_certificate_authorities" {
-  description = "All api_management_gateway_certificate_authority resources"
-  value       = azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities
-}
 output "api_management_gateway_certificate_authorities_api_management_id" {
-  description = "List of api_management_id values across all api_management_gateway_certificate_authorities"
-  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_gateway_certificate_authorities, keyed the same as var.api_management_gateway_certificate_authorities"
+  value       = { for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : k => v.api_management_id }
 }
 output "api_management_gateway_certificate_authorities_certificate_name" {
-  description = "List of certificate_name values across all api_management_gateway_certificate_authorities"
-  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.certificate_name]
+  description = "Map of certificate_name values across all api_management_gateway_certificate_authorities, keyed the same as var.api_management_gateway_certificate_authorities"
+  value       = { for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : k => v.certificate_name }
 }
 output "api_management_gateway_certificate_authorities_gateway_name" {
-  description = "List of gateway_name values across all api_management_gateway_certificate_authorities"
-  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.gateway_name]
+  description = "Map of gateway_name values across all api_management_gateway_certificate_authorities, keyed the same as var.api_management_gateway_certificate_authorities"
+  value       = { for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : k => v.gateway_name }
 }
 output "api_management_gateway_certificate_authorities_is_trusted" {
-  description = "List of is_trusted values across all api_management_gateway_certificate_authorities"
-  value       = [for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : v.is_trusted]
+  description = "Map of is_trusted values across all api_management_gateway_certificate_authorities, keyed the same as var.api_management_gateway_certificate_authorities"
+  value       = { for k, v in azurerm_api_management_gateway_certificate_authority.api_management_gateway_certificate_authorities : k => v.is_trusted }
 }
 
